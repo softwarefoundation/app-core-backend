c00958ed6f1b0782cae8b27a0f9daaeaf52be9cb
Dherkyan R. Silva <dherkyan@gmail.com>
2024-03-16 14:02:36 -0300

    Add Lombok

--- src/main/java/br/com/devchampions/backendapp/controller/CpfController.java
+++ src/main/java/br/com/devchampions/backendapp/controller/CpfController.java
@@ -1,6 +1,7 @@
 package br.com.devchampions.backendapp.controller;
 
 import br.com.devchampions.backendapp.dto.CpfDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +9,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.Pattern;
+
+@Slf4j
 @RestController
 @RequestMapping("/cpf")
 public class CpfController {
 
-    @PostMapping
-    public ResponseEntity<?> cpf(@RequestBody CpfDto cpfDto){
+    @PostMapping("validar")
+    public ResponseEntity<?> validarCpf(@RequestBody CpfDto cpfDto) {
 
-        if(cpfDto.getCpf() == null){
+        log.info("CPF: {}", cpfDto.getCpf());
+
+        if (cpfDto.getCpf() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Número de CPF obrigatório");
+        }
+
+        if (cpfDto.getCpf().trim().length() != 11) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O CPF deve conter 11 dígitos");
         }
 
-        return ResponseEntity.ok(cpfDto);
+        // Somente números
+        Pattern p = Pattern.compile("[^0-9]+");
+
+        if (p.matcher(cpfDto.getCpf()).find()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Informe somente os números do CPF");
+        }
+
+        return ResponseEntity.ok(cpfDto.getCpf());
     }
+
 
 
 }
