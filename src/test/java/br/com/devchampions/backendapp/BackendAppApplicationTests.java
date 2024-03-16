73d868669f0b33cbbc472a7db0f5f268c80f4610
Dherkyan R. Silva <dherkyan@gmail.com>
2024-03-16 05:06:24 -0300

    Projeto Backend APP

---
+++ src/test/java/br/com/devchampions/backendapp/BackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devchampions.backendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
